581a19162b1fb1964a35204cf6f30775a0f66c9b
tonie <poet@123>
2025-04-30 13:22:01 +0300

    simple js arithmetic

---
+++ attempt.js
@@ -0,0 +1,6 @@
+const pi = 24;
+var myName= 'Anthony';
+
+myBrethren= `am Ngunjiri  ${myName} , a software engineer and a ${pi} hrs developer, lets go techie ,shall we?`
+console.log(myBrethren);
+ 
