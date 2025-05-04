69d00d7829055d4d6a138198ba6d15c8b9c09e36
tonie <poet@123>
2025-05-04 19:14:12 +0300

    add objects

--- attempt.js
+++ attempt.js
@@ -5,16 +5,31 @@
 // console.log(myBrethren);
  
 //Working with operation structures
- let hours ='';
-const Name = 'Tonie parker';
-  if(hours ? 6 : 12){
-    console.log(`good morning ${Name} `)
-  }else if(hours ? 13 : 17){
-    console.log(`good afternoon ${Name} `)
+//  let hours ='';
+// const Name = 'Tonie parker';
+//   if(hours ? 6 : 12){
+//     console.log(`good morning ${Name} `)
+//   }else if(hours ? 13 : 17){
+//     console.log(`good afternoon ${Name} `)
 
-  }else{
-    console.log(`good night ${Name} `)
+//   }else{
+//     console.log(`good night ${Name} `)
 
-  }
+//   }
+
+
+//objects
+
+const product = {
+  name: 'socks',
+  price:150
+};
+console.log(product);
+console.log(product.name);
+
+//modify objects
+product.price = 120;
+
+console.log(product.price);
 
  
