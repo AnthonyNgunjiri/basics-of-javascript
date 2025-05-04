183541a5eba0dd79cafa66f7c0111350d4f18bc0
tonie <poet@123>
2025-05-04 12:07:30 +0300

    remove repetition

--- attempt.js
+++ attempt.js
@@ -1,6 +1,20 @@
-const pi = 24;
-var myName= 'Anthony';
+// const pi = 24;
+// var myName= 'Anthony';
 
-myBrethren= `am Ngunjiri  ${myName} , a software engineer and a ${pi} hrs developer, lets go techie ,shall we?`
-console.log(myBrethren);
+// myBrethren= `am Ngunjiri  ${myName} , a software engineer and a ${pi} hrs developer, lets go techie ,shall we?`
+// console.log(myBrethren);
+ 
+//Working with operation structures
+ let hours ='';
+const Name = 'Tonie parker';
+  if(hours ? 6 : 12){
+    console.log(`good morning ${Name} `)
+  }else if(hours ? 13 : 17){
+    console.log(`good afternoon ${Name} `)
+
+  }else{
+    console.log(`good night ${Name} `)
+
+  }
+
  
